14560f8840f15750d683edf77190d63cb0d0fc9e
14ams18 <abigael.schonewille@hotmail.com>
2016-11-24 21:35:27 -0500

    Update main.c

--- main.c
+++ main.c
@@ -9,10 +9,13 @@ int main() {
         int n = 2;
         char target[n];
         char *c = safeststrcat(a,b);
+        printf("Result of strcat: %s\n", c);
         if (substring(a, from, n, target) == 0)
-                printf("Result of substring: %s", target);
+                printf("Result of substring: %s\n", target);
         else
                 printf("Failure of substring");
                 exit(-1);
         return 0;
+
 }
+
