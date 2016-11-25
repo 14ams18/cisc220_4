ec60abdafd868387b1762f904db81a352371d327
14ams18 <abigael.schonewille@hotmail.com>
2016-11-24 21:09:26 -0500

    Update mystring.c

--- mystring.c
+++ mystring.c
@@ -1,1 +1,29 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include "mystring.h"
 
+char* safeststrcat(char *a, char *b) {
+        int len = strlen(a) + strlen(b);
+        char *c = (char *)malloc(sizeof(char)*len);
+        c = strcat(c,a);
+        c = strcat(c,b);
+        return c;
+}
+
+int substring(char *source, int from, int n, char *target) {
+        int length = (strlen(source));
+        if ((length - from) > n && from < length) {
+                char temp[n];
+                strcpy(temp, source);
+                for (int i=0; i<n; i++) {
+                        target[i] = temp[from+i];
+                        printf("target elems %c", target[i]);
+                }
+        }
+        else {
+                printf("The input values were invalid");
+                return -1;
+        }
+        return 0;
+}
