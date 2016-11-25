e8d1c0bd42d8595f20516f8801befc5d015a0906
14ams18 <abigael.schonewille@hotmail.com>
2016-11-24 23:08:06 -0500

    Update matrixMult.c

--- matrixMult.c
+++ matrixMult.c
@@ -6,10 +6,6 @@ typedef struct Matrix{
         int height;
 } matrix;
 
-typedef struct array{
-        int array[][];
-} array;
-
 int checkDimension (matrix m1, matrix m2, matrix result) {
         if (m1.width == m2.height) {
                 result.width = m2.width;
@@ -21,12 +17,11 @@ int checkDimension (matrix m1, matrix m2, matrix result) {
         };
 }
 
-array matrixMult (matrix m1, matrix m2, int[][] m1Val, int[][] m2Val) {
-        array resultVal.array[result.height][result.width];
+*int matrixMult (matrix m1, matrix m2, int[][] m1Val, int[][] m2Val, *int resultval) {
         for ( int i=0; i<m1.height; i++) {
                 for ( int j=0; j<m2.width; j++) {
                         for ( int k=0; k<m1.width; k++) {
-                                resultVal[i][j] += m1Val[i][k] * m2Val[k][j];
+                                *resultVal[i][j] += m1Val[i][k] * m2Val[k][j];
                         }
                 }
         }
@@ -50,25 +45,23 @@ int main () {
                         fprintf(stderr, "Dimension Mismatch");
                 }
         }
-        int m1Val[m1.height][m1.width];
+      int m1Val[m1.height][m1.width];
         int m2Val[m2.height][m2.width];
+        int resultVal[result.height][result.width];
         if (scanf("Input matrix 1 elements separated by spaces: %d", &m1Val[m1.height][m1.width]) && scanf("Input matrix 2 elements separated by spaces: %d", &m2Val[m2.height][m2.width])) {
-        array = matrixMult(m1, m2, m1Val, m2Val, resultVal);
+        int* rptr = resultVal;
+        rptr = matrixMult(m1, m2, m1Val, m2Val, resultVal);
         }
         else {
                 printf("Invalid Input.");
                 fprintf(stderr, "Invalid Input");
-                free(m1);
-                free(m2);
-                free(result);
                 exit(-1);
         }
         for(int i = 0; i < result.height; i++) {
             for(int j = 0; j < result.width; j++) {
-                printf("%d ", resultVal.array[i][j]);
+                printf("%d ", resultVal[i][j]);
             }
             printf("\n");
         }
         return 0;
 }
-
